051e079cffe6eb83fbea16b5dafb1c4a6af4d104
Temirlan Baibolov <temirlan.baibolov@leaguesofcode.com>
2022-06-22 00:38:41 +0200

    add web server

---
+++ hw-3/server/server.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const port = 3000;
+
+const delay = (ms) => {
+  return new Promise((resolve) => {
+    const timer = setTimeout(() => {
+      clearTimeout(timer);
+      resolve(true);
+    }, ms);
+  });
+};
+
+const server = http.createServer(async (request, response) => {
+  await delay(100);
+  let body = [];
+
+  request
+    .on("data", (chunk) => {
+      body.push(chunk);
+    })
+    .on("end", () => {
+      if (body.length === 0) {
+        body = "Empty request body\n";
+      } else {
+        body = Buffer.concat(body).toString();
+      }
+
+      response.setHeader("Content-Type", "text/plain");
+      response.end(body);
+    });
+});
+
+server.listen(port, () => {
+  console.log(`server listening at port ${port}`);
+});
