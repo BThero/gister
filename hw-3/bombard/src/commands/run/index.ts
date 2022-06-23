5c56e24c0f6174db08786df2b216da21f9961aa2
Temirlan Baibolov <temirlan.baibolov@leaguesofcode.com>
2022-06-24 00:14:49 +0200

    finish task

--- hw-3/bombard/src/commands/run/index.ts
+++ hw-3/bombard/src/commands/run/index.ts
@@ -1,11 +1,54 @@
 import { Command, Flags } from "@oclif/core";
+import axios from "axios";
+
+function randstr(length: number) {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+function* requestGenerator(
+  n: number,
+  url: string,
+  withBody: boolean,
+  zeroTime: number
+): any {
+  for (let i = 0; i < n; i += 1) {
+    yield (callback: any) => {
+      const startTime = Date.now();
+
+      axios
+        .get(url, {
+          data: withBody ? randstr(10_000) : null,
+        })
+        .then((res: any) => {
+          const endTime = Date.now();
+
+          callback(null, {
+            call: startTime - zeroTime,
+            duration: endTime - startTime,
+            data: res.data,
+          });
+        })
+        .catch((error: any) => {
+          callback(null, { error: error });
+        });
+    };
+  }
+}
 
 export default class Run extends Command {
-  static description = "Say hello";
+  static description = "Run bombard";
 
   static examples = [
-    `$ oex hello friend --from oclif
-hello friend from oclif! (./src/commands/hello/index.ts)
+    `$ oex run http://localhost:3000 (./src/commands/run/index.ts)
 `,
   ];
 
@@ -38,73 +81,45 @@ hello friend from oclif! (./src/commands/hello/index.ts)
 
   async run(): Promise<void> {
     const { args, flags } = await this.parse(Run);
-    const http = require("http");
     const async = require("async");
 
     const reqCount = flags.requests ?? 10;
     const concurr = flags.concurrency ?? 4;
-    // const withBody = flags.body;
+    const withBody = flags.body;
 
     const url = args.url;
-    const requests = [];
     const zeroTime = Date.now();
-
-    for (let i = 0; i < reqCount; i += 1) {
-      requests.push((callback: any) => {
-        const startTime = Date.now();
-
-        http
-          .get(url, (res: any) => {
-            let data = "";
-
-            // A chunk of data has been received.
-            res.on("data", (chunk: any) => {
-              data += chunk;
-            });
-
-            // The whole response has been received. Print out the result.
-            res.on("end", () => {
-              const endTime = Date.now();
-
-              callback(null, {
-                call: startTime - zeroTime,
-                duration: endTime - startTime,
-                data: data,
-              });
-            });
-          })
-          .on("error", (error: any) => {
-            callback(null, { error: error });
-          });
-      });
-    }
 
     let success = 0;
     let fail = 0;
     let avg = 0;
 
-    async.parallelLimit(requests, concurr, (err: any, results: any) => {
-      if (err) {
-        this.log("Some error occurred");
-      } else {
-        for (const result of results) {
-          const { duration, error } = result;
+    async.parallelLimit(
+      [...requestGenerator(reqCount, url, withBody, zeroTime)],
+      concurr,
+      (err: any, results: any) => {
+        if (err) {
+          this.log("Some error occurred");
+        } else {
+          for (const result of results) {
+            const { duration, error } = result;
 
-          if (error) {
-            fail += 1;
-          } else {
-            success += 1;
-            avg += duration;
+            if (error) {
+              fail += 1;
+            } else {
+              success += 1;
+              avg += duration;
+            }
+          }
+
+          this.log(`bombarded ${reqCount} times`);
+          this.log(`${success} successful, ${fail} failing`);
+
+          if (success > 0) {
+            this.log(`average response time ${avg / success}ms`);
           }
         }
-
-        this.log(`bombarded ${reqCount} times`);
-        this.log(`${success} successful, ${fail} failing`);
-
-        if (success > 0) {
-          this.log(`average response time ${avg / success}ms`);
-        }
       }
-    });
+    );
   }
 }
