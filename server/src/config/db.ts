cb3cd5de796af410759b08e0dae467c40ddb9de9
Temirlan Baibolov <bthero09@gmail.com>
2024-03-19 11:48:55 +0700

    feat: add local mongodb docker compose

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,11 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-	const conn = await mongoose.connect(process.env.MONGO_URI);
+  const conn = await mongoose.connect(process.env.MONGO_URI);
 };
 
 const disconnectDB = async () => {
-	await mongoose.disconnect();
+  await mongoose.disconnect();
 };
 
 export { connectDB, disconnectDB };
